78ec663e4bda1d6af4e078bc08c1f37081e66505
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-02-16 18:21:11 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/TestRunner.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/TestRunner.cs
@@ -21,6 +21,7 @@ namespace Apache.Ignite.Core.Tests
     using System.Diagnostics;
     using System.Reflection;
     using Apache.Ignite.Core.Tests.Cache.Query;
+    using Apache.Ignite.Core.Tests.DataStructures;
     using Apache.Ignite.Core.Tests.Memory;
     using NUnit.ConsoleRunner;
 
@@ -32,7 +33,7 @@ namespace Apache.Ignite.Core.Tests
             Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
             Debug.AutoFlush = true;
 
-            TestOne(typeof(IgniteConfigurationTest), "TestStaticIpFinder");
+            TestOne(typeof(QueueTest), "TestConfiguration");
 
             //TestAll(typeof (CacheQueriesCodeConfigurationTest));
             //TestAllInAssembly();
