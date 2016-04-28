3df0158e50a1df55880a82362e8adcba1eec63e6
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-04-28 12:34:28 +0300

    wip

--- modules/platforms/dotnet/changeVersions.ps1
+++ modules/platforms/dotnet/changeVersions.ps1
@@ -1,4 +1,6 @@
-﻿[System.IO.Directory]::GetFiles('C:\W\incubator-ignite\modules\platforms\dotnet', "AssemblyInfo.cs", [System.IO.SearchOption]::AllDirectories) `
+﻿$num = 13
+
+[System.IO.Directory]::GetFiles('C:\W\incubator-ignite\modules\platforms\dotnet', "AssemblyInfo.cs", [System.IO.SearchOption]::AllDirectories) `
     | ForEach-Object {
-        (Get-Content $_) -replace '1', '2' | Out-File $_ -Encoding utf8
+        (Get-Content $_) -replace 'AssemblyVersion\("(\d+\.\d+\.\d+).*?"\)', 'AssemblyVersion("$1")' | Out-File $_ -Encoding utf8
       }
