5184eaf1c262e013a128452ee3f04eded1f75e63
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-04-28 12:30:05 +0300

    wip

---
+++ modules/platforms/dotnet/changeVersions.ps1
@@ -0,0 +1,4 @@
+﻿[System.IO.Directory]::GetFiles('C:\W\incubator-ignite\modules\platforms\dotnet', "AssemblyInfo.cs", [System.IO.SearchOption]::AllDirectories) `
+    | ForEach-Object {
+        (Get-Content $_) -replace '1', '2' | Out-File $_ -Encoding utf8
+      }
