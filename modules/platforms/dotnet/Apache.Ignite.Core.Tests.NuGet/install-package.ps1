36417af81e4ef4ebd11f01dc544e3528aadf0060
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-04-28 13:41:38 +0300

    Fix version in NuGet tests

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/install-package.ps1
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests.NuGet/install-package.ps1
@@ -13,7 +13,7 @@ mkdir pkg
 & $ng pack ..\Apache.Ignite.Core\Apache.Ignite.Core.csproj -Prop Configuration=Release -OutputDirectory nupkg
 & $ng pack ..\Apache.Ignite.Linq\Apache.Ignite.Linq.csproj -Prop Configuration=Release -OutputDirectory nupkg
 
-$ver = (Get-ChildItem nupkg\Apache.Ignite.Linq*)[0].Name -replace '\D+([\d.]+)\.\D+','$1'
+$ver = (Get-ChildItem nupkg\Apache.Ignite.Linq*)[0].Name -replace 'Apache\.Ignite\.Linq\.(.*?)\.nupkg', '$1'
 
 # Replace versions in project files
 (Get-Content packages.config) `
