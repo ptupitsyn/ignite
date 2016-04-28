8fc6193d7994c6debfd8e125f75e7159670023f8
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-04-28 13:16:40 +0300

    ps script done

--- modules/platforms/dotnet/changeVersions.ps1
+++ modules/platforms/dotnet/changeVersions.ps1
@@ -1,11 +1,15 @@
 ﻿# Number of days since 4/28/2016
 $buildNum = ([System.DateTime]::Now - [System.DateTime]::FromBinary(-8587399024854775808)).Days
 
+echo "Updating versions to $buildnum-nightly"
+
 Get-ChildItem -Filter AssemblyInfo.cs -Recurse `
     | ForEach-Object {
-        (Get-Content $_.FullName) `
+        $file = $_.FullName
+        echo "Updating $file..."
+        (Get-Content $file) `
             -replace 'AssemblyVersion\("(\d+\.\d+\.\d+).*?"\)', ('AssemblyVersion("$1.' + $buildNum + '")') `
             -replace 'AssemblyFileVersion\("(\d+\.\d+\.\d+).*?"\)', ('AssemblyFileVersion("$1.' + $buildNum + '")') `
             -replace 'AssemblyInformationalVersion\("(\d+\.\d+\.\d+).*?"\)', ('AssemblyInformationalVersion("$1.' + $buildNum + '-nightly")') `
-            | Out-File $_.FullName -Encoding utf8
+            | Out-File $file -Encoding utf8
       }
