8e59bd9790c8922b8cb5f8c76033c8211278e26d
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-04-28 12:46:10 +0300

    wip

--- modules/platforms/dotnet/changeVersions.ps1
+++ modules/platforms/dotnet/changeVersions.ps1
@@ -3,8 +3,8 @@
 [System.IO.Directory]::GetFiles('C:\W\incubator-ignite\modules\platforms\dotnet', "AssemblyInfo.cs", [System.IO.SearchOption]::AllDirectories) `
     | ForEach-Object {
         (Get-Content $_) `
-            -replace 'AssemblyVersion\("(\d+\.\d+\.\d+).*?"\)', 'AssemblyVersion("$1.' + $num + '")' `
-            -replace 'AssemblyFileVersion\("(\d+\.\d+\.\d+).*?"\)', 'AssemblyFileVersion("$1.' + $num + '")' `
-            -replace 'AssemblyInformationalVersion\("(\d+\.\d+\.\d+).*?"\)', 'AssemblyInformationalVersion("$1.' + $num + '-nightly")' `
+            -replace 'AssemblyVersion\("(\d+\.\d+\.\d+).*?"\)', ('AssemblyVersion("$1.' + $num + '")') `
+            -replace 'AssemblyFileVersion\("(\d+\.\d+\.\d+).*?"\)', ('AssemblyFileVersion("$1.' + $num + '")') `
+            -replace 'AssemblyInformationalVersion\("(\d+\.\d+\.\d+).*?"\)', ('AssemblyInformationalVersion("$1.' + $num + '-nightly")') `
             | Out-File $_ -Encoding utf8
       }
