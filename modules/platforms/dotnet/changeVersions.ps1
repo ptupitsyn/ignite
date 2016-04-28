e1621b8f65c323cfe64c3cf78b6ecb1bed9e289f
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2016-04-28 13:04:20 +0300

    ps script done

--- modules/platforms/dotnet/changeVersions.ps1
+++ modules/platforms/dotnet/changeVersions.ps1
@@ -1,9 +1,9 @@
 ﻿# Number of days since 4/28/2016
 $buildNum = ([System.DateTime]::Now - [System.DateTime]::FromBinary(-8587399024854775808)).Days
 
-[System.IO.Directory]::GetFiles('C:\W\incubator-ignite\modules\platforms\dotnet', "AssemblyInfo.cs", [System.IO.SearchOption]::AllDirectories) `
+Get-ChildItem -Filter AssemblyInfo.cs -Recurse `
     | ForEach-Object {
-        (Get-Content $_) `
+        (Get-Content $_.FullName) `
             -replace 'AssemblyVersion\("(\d+\.\d+\.\d+).*?"\)', ('AssemblyVersion("$1.' + $buildNum + '")') `
             -replace 'AssemblyFileVersion\("(\d+\.\d+\.\d+).*?"\)', ('AssemblyFileVersion("$1.' + $buildNum + '")') `
             -replace 'AssemblyInformationalVersion\("(\d+\.\d+\.\d+).*?"\)', ('AssemblyInformationalVersion("$1.' + $buildNum + '-nightly")') `
