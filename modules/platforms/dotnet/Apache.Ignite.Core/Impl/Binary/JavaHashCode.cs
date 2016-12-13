59a3735a3b9af2119a1abca68e231f4d8df8f4b0
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-12-13 19:48:51 +0300

    wip decimal

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Binary/JavaHashCode.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Binary/JavaHashCode.cs
@@ -18,6 +18,9 @@
 namespace Apache.Ignite.Core.Impl.Binary
 {
     using System;
+    using System.IO;
+    using Apache.Ignite.Core.Impl.Binary.IO;
+    using Apache.Ignite.Core.Impl.Memory;
 
     /// <summary>
     /// Java hash code implementations for basic types.
@@ -72,7 +75,25 @@ namespace Apache.Ignite.Core.Impl.Binary
 
                 if (val is decimal)
                 {
-                    // TODO:
+                    var dc = (decimal) val;
+
+                    if (dc < long.MaxValue)
+                    {
+                        // TODO: Scale?
+                        return 31*((long) dc).GetHashCode() + 20;
+                    }
+
+                    var stream = new BinaryHeapStream(20);
+                    BinaryUtils.WriteDecimal((decimal)val, stream);
+
+                    stream.Seek(0, SeekOrigin.Begin);
+
+                    var scale = stream.ReadInt();
+                    var len = stream.ReadInt();
+                    var mag = stream.ReadByteArray(len);
+
+                    // TODO
+                    return scale.GetHashCode() + mag[0].GetHashCode();
                 }
 
                 if (val is sbyte)
