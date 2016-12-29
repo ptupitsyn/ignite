53ce132c06e9a52a6d02b32acc060c6b0568b2c6
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-12-29 17:06:00 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/CacheAbstractTransactionalTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/CacheAbstractTransactionalTest.cs
@@ -611,12 +611,16 @@ namespace Apache.Ignite.Core.Tests.Cache
         {
             CheckTxOp((cache, key) => cache.Put(key, -5));
             CheckTxOp((cache, key) => cache.PutAsync(key, -5).Wait());
+
             CheckTxOp((cache, key) => cache.PutAll(new Dictionary<int, int> {{key, -7}}));
             CheckTxOp((cache, key) => cache.PutAllAsync(new Dictionary<int, int> {{key, -7}}).Wait());
+
             CheckTxOp((cache, key) => cache.RemoveAll(new[] {key}));
             CheckTxOp((cache, key) => cache.RemoveAllAsync(new[] {key}).Wait());
+
             CheckTxOp((cache, key) => cache.GetAndPut(key, -9));
             CheckTxOp((cache, key) => cache.GetAndPutAsync(key, -9).Wait());
+
             CheckTxOp((cache, key) =>
             {
                 cache.Remove(key);
@@ -627,13 +631,18 @@ namespace Apache.Ignite.Core.Tests.Cache
                 cache.Remove(key);
                 cache.GetAndPutIfAbsentAsync(key, -10).Wait();
             });
+
             CheckTxOp((cache, key) => cache.GetAndRemove(key));
             CheckTxOp((cache, key) => cache.GetAndRemoveAsync(key));
+
             CheckTxOp((cache, key) => cache.GetAndReplace(key, -11));
             CheckTxOp((cache, key) => cache.GetAndReplaceAsync(key, -11));
 
             CheckTxOp((cache, key) => cache.Invoke(key, new AddProcessor(), 1));
             CheckTxOp((cache, key) => cache.InvokeAsync(key, new AddProcessor(), 1));
+
+            CheckTxOp((cache, key) => cache.InvokeAll(new[] {key}, new AddProcessor(), 1));
+            CheckTxOp((cache, key) => cache.InvokeAllAsync(new[] {key}, new AddProcessor(), 1));
         }
 
         /// <summary>
