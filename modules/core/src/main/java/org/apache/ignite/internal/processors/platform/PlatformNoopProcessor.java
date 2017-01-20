c59a74eb4d12240e62a78c841d81201af910c15a
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-01-20 19:15:58 +0300

    Fixing merge

--- modules/core/src/main/java/org/apache/ignite/internal/processors/platform/PlatformNoopProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/platform/PlatformNoopProcessor.java
@@ -153,16 +153,6 @@ public class PlatformNoopProcessor extends GridProcessorAdapter implements Platf
     }
 
     /** {@inheritDoc} */
-    @Override public boolean registerType(int id, String name) {
-        return false;
-    }
-
-    /** {@inheritDoc} */
-    @Override public String getClass(int id) {
-        return null;
-    }
-
-    /** {@inheritDoc} */
     @Override public void getCacheNames(long memPtr) {
         // No-op.
     }
