4c3e28062ca7782e139c096137c5474796e753df
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-01-31 14:25:30 +0300

    SerializableTest wip

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Binary/SerializableTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Binary/SerializableTest.cs
@@ -17,6 +17,11 @@
 
 namespace Apache.Ignite.Core.Tests.Binary
 {
+    using System;
+    using System.Runtime.Serialization;
+    using Apache.Ignite.Core.Impl.Binary;
+    using NUnit.Framework;
+
     /// <summary>
     /// Tests [Serializable] mechanism handling.
     /// </summary>
@@ -28,5 +33,61 @@ namespace Apache.Ignite.Core.Tests.Binary
         // with ISerializable
         // without ISerializable
         // check built-in types: collections, delegates, types, memberInfos, etc
+
+
+        /// <summary>
+        /// Tests that primitive types can be serialized with ISerializable mechanism.
+        /// </summary>
+        [Test]
+        public void TestPrimitives()
+        {
+            var marsh = new Marshaller(null);
+
+            var val = new Primitives
+            {
+                Byte = 1
+            };
+
+            Assert.IsFalse(val.GetObjectDataCalled);
+            Assert.IsFalse(val.SerializationCtorCalled);
+
+            var res = marsh.Unmarshal<Primitives>(marsh.Marshal(val));
+
+            Assert.IsTrue(val.GetObjectDataCalled);
+            Assert.IsFalse(val.SerializationCtorCalled);
+
+            Assert.IsFalse(res.GetObjectDataCalled);
+            Assert.IsTrue(res.SerializationCtorCalled);
+
+            Assert.AreEqual(val.Byte, res.Byte);
+        }
+
+        [Serializable]
+        private class Primitives : ISerializable
+        {
+            public bool GetObjectDataCalled { get; private set; }
+            public bool SerializationCtorCalled { get; private set; }
+
+            public byte Byte { get; set; }
+
+            public Primitives()
+            {
+                // No-op.
+            }
+
+            public Primitives(SerializationInfo info, StreamingContext context)
+            {
+                SerializationCtorCalled = true;
+
+                Byte = info.GetByte("byte");
+            }
+
+            public void GetObjectData(SerializationInfo info, StreamingContext context)
+            {
+                GetObjectDataCalled = true;
+
+                info.AddValue("byte", Byte);
+            }
+        }
     }
 }
