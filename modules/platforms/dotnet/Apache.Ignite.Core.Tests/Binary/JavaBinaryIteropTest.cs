bbfff463d22821122f4b769b26580aefef5cf707
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-02-07 14:27:07 +0300

    Add failing test for IGNITE-4663

---
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Binary/JavaBinaryIteropTest.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Tests.Binary
+{
+    using System;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests binary type interoperability between .NET and Java code.
+    /// </summary>
+    public class JavaBinaryIteropTest
+    {
+        /// <summary>
+        /// Tests that all kinds of values from .NET can be handled properly on Java side.
+        /// </summary>
+        [Test]
+        public void TestValueRoundtrip()
+        {
+            using (var ignite = Ignition.Start(TestUtils.GetTestConfiguration()))
+            {
+                var cache = ignite.CreateCache<int, object>("TestValueRoundtrip");
+
+                var guids = new[] {Guid.Empty, Guid.NewGuid()};
+                cache[1] = guids;
+                Assert.AreEqual(guids, cache[1]);
+
+                // TODO: All array types, nullable and not, with custom objects, unregistered types, etc.
+            }
+        }
+
+        /// <summary>
+        /// Tests that arrays of any types are understood on Java side.
+        /// </summary>
+        [Test]
+        public void TestArrays()
+        {
+            // TODO: ?
+        }
+    }
+}
