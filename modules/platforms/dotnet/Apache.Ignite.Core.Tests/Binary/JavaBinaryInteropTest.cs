b27077c666208d00e9dfaa68dd945408a09eb0e8
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-02-10 13:58:51 +0300

    wip tests

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/Binary/JavaBinaryInteropTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Binary/JavaBinaryInteropTest.cs
@@ -20,6 +20,7 @@ namespace Apache.Ignite.Core.Tests.Binary
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using Apache.Ignite.Core.Cache;
     using NUnit.Framework;
 
     /// <summary>
@@ -35,26 +36,59 @@ namespace Apache.Ignite.Core.Tests.Binary
         {
             using (var ignite = Ignition.Start(TestUtils.GetTestConfiguration()))
             {
-                var cache = ignite.CreateCache<int, object>("TestValueRoundtrip");
+                var cache = ignite.CreateCache<int, object>((string) null);
 
-                // TODO
                 // Basic types.
+                CheckValueCaching((byte) 255);
+                CheckValueCachingAsObject((byte) 255);
+
+                CheckValueCaching((sbyte) -10);
+
+                CheckValueCaching((short) -32000);
+                CheckValueCachingAsObject((short) -32000);
+
+                CheckValueCaching((ushort) 65350);
+                CheckValueCaching(int.MinValue);
+                CheckValueCaching(uint.MaxValue);
+                CheckValueCaching(long.MinValue);
+                CheckValueCaching(ulong.MaxValue);
 
                 // Basic type arrays.
-                var guids = new[] {Guid.Empty, Guid.NewGuid()};
-                cache[1] = guids;
-                Assert.AreEqual(guids, cache[1]);
+                CheckValueCaching(new[] {Guid.Empty, Guid.NewGuid()});
+                CheckValueCachingAsObject(new[] {Guid.Empty, Guid.NewGuid()});
 
                 // Custom types.
-                cache[1] = new Foo {X = 10};
-                Assert.AreEqual(10, ((Foo) cache[1]).X);
+                CheckValueCaching(new Foo {X = 10});
+                CheckValueCachingAsObject(new Foo {X = 10});
 
-                cache[1] = new List<Foo> {new Foo {X = -2}, new Foo {X = 2}};
-                Assert.AreEqual(new[] {-2, 2}, ((List<Foo>) cache[1]).Select(x => x.X));
+                CheckValueCaching(new List<Foo> {new Foo {X = -2}, new Foo {X = 2}});
+                CheckValueCachingAsObject(new List<Foo> {new Foo {X = -2}, new Foo {X = 2}});
 
-                cache[1] = new[] {new Foo {X = -1}, new Foo {X = 1}};
-                Assert.AreEqual(new[] {-1, 1}, ((Foo[]) cache[1]).Select(x => x.X));
+                CheckValueCaching(new[] {new Foo {X = -1}, new Foo {X = 1}});
+                //CheckValueCachingAsObject(new[] {new Foo {X = -1}, new Foo {X = 1}});
             }
+        }
+
+        /// <summary>
+        /// Checks caching of a value.
+        /// </summary>
+        private static void CheckValueCaching<T>(T val)
+        {
+            var cache = Ignition.GetIgnite(null).GetCache<int, T>(null);
+
+            cache[1] = val;
+            Assert.AreEqual(val, cache[1]);
+        }
+
+        /// <summary>
+        /// Checks caching of a value.
+        /// </summary>
+        private static void CheckValueCachingAsObject<T>(T val)
+        {
+            var cache = Ignition.GetIgnite(null).GetCache<int, object>(null);
+
+            cache[1] = val;
+            Assert.AreEqual(val, (T) cache[1]);
         }
 
         /// <summary>
@@ -63,6 +97,38 @@ namespace Apache.Ignite.Core.Tests.Binary
         private class Foo
         {
             public int X { get; set; }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return X == ((Foo) obj).X;
+            }
+
+            public override int GetHashCode()
+            {
+                return X;
+            }
+        }
+
+        /// <summary>
+        /// Test custom struct.
+        /// </summary>
+        private struct Bar
+        {
+            public int X { get; set; }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                return obj is Bar && X == ((Bar) obj).X;
+            }
+
+            public override int GetHashCode()
+            {
+                return X;
+            }
         }
     }
 }
