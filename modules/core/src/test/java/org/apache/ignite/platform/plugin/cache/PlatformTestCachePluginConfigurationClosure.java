0c5d38f43c481c46d0f5fc06cd1f97eebba3f19b
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-02-17 14:51:07 +0300

    wip tests

--- modules/core/src/test/java/org/apache/ignite/platform/plugin/cache/PlatformTestCachePluginConfigurationClosure.java
+++ modules/core/src/test/java/org/apache/ignite/platform/plugin/cache/PlatformTestCachePluginConfigurationClosure.java
@@ -19,7 +19,11 @@ package org.apache.ignite.platform.plugin.cache;
 
 import org.apache.ignite.binary.BinaryRawReader;
 import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.plugin.CachePluginConfiguration;
 import org.apache.ignite.plugin.platform.PlatformCachePluginConfigurationClosure;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Test config closure.
@@ -27,6 +31,18 @@ import org.apache.ignite.plugin.platform.PlatformCachePluginConfigurationClosure
 public class PlatformTestCachePluginConfigurationClosure implements PlatformCachePluginConfigurationClosure {
     /** {@inheritDoc} */
     @Override public void apply(CacheConfiguration cacheConfiguration, BinaryRawReader reader) {
+        ArrayList<CachePluginConfiguration> cfgs = new ArrayList<>();
 
+        if (cacheConfiguration.getPluginConfigurations() != null) {
+            Collections.addAll(cfgs, cacheConfiguration.getPluginConfigurations());
+        }
+
+        PlatformTestCachePluginConfiguration plugCfg = new PlatformTestCachePluginConfiguration();
+
+        plugCfg.setPluginProperty(reader.readString());
+
+        cfgs.add(plugCfg);
+
+        cacheConfiguration.setPluginConfigurations(cfgs.toArray(new CachePluginConfiguration[cfgs.size()]));
     }
 }
