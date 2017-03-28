b78def417898a8870918f1d39488489eaad85f1a
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-03-28 15:31:55 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Binary/BinarizableSerializer.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Binary/BinarizableSerializer.cs
@@ -35,8 +35,11 @@ namespace Apache.Ignite.Core.Impl.Binary
         /** <inheritdoc /> */
         public void WriteBinary<T>(T obj, BinaryWriter writer)
         {
-            // TODO: Pass a wrapper over writer which sorts fields.
-            ((IBinarizable) obj).WriteBinary(writer);
+            var sorter = new SortedBinaryWriter(writer);
+            
+            ((IBinarizable) obj).WriteBinary(sorter);
+
+            sorter.Flush();
         }
 
         /** <inheritdoc /> */
