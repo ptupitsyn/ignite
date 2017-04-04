0575e7fe875c63a1da31d0fe49940757609eb424
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-04-04 11:26:39 +0300

    Fix reconnect example to use static discovery

--- modules/platforms/dotnet/examples/Apache.Ignite.Examples/Misc/ClientReconnectExample.cs
+++ modules/platforms/dotnet/examples/Apache.Ignite.Examples/Misc/ClientReconnectExample.cs
@@ -50,12 +50,15 @@ namespace Apache.Ignite.Examples.Misc
             var evt = new ManualResetEvent(false);
             ThreadPool.QueueUserWorkItem(_ => RunServer(evt));
 
-            // Wait a bit for server to begin startup to avoid race on ClientMode flag.
-            Thread.Sleep(100);
+            // Wait a moment for server to begin startup.
+            Thread.Sleep(200);
 
-            Ignition.ClientMode = true;
+            var cfg = new IgniteConfiguration(GetIgniteConfiguration())
+            {
+                ClientMode = true
+            };
 
-            using (var ignite = Ignition.StartFromApplicationConfiguration())
+            using (var ignite = Ignition.Start(cfg))
             {
                 Console.WriteLine(">>> Client node connected to the cluster.");
 
@@ -113,19 +116,10 @@ namespace Apache.Ignite.Examples.Misc
         /// <param name="evt"></param>
         private static void RunServer(WaitHandle evt)
         {
-            var cfg = new IgniteConfiguration
+            var cfg = new IgniteConfiguration(GetIgniteConfiguration())
             {
                 // Nodes within a single process are distinguished by GridName property.
                 IgniteInstanceName = "serverNode",
-
-                // Discovery settings are the same as in app.config.
-                DiscoverySpi = new TcpDiscoverySpi
-                {
-                    IpFinder = new TcpDiscoveryStaticIpFinder
-                    {
-                        Endpoints = new[] {"127.0.0.1:47500"}
-                    }
-                },
 
                 CacheConfiguration = new[] {new CacheConfiguration(CacheName)},
 
@@ -157,6 +151,23 @@ namespace Apache.Ignite.Examples.Misc
             {
                 evt.WaitOne();
             }
+        }
+
+        /// <summary>
+        /// Gets the base Ignite configuration.
+        /// </summary>
+        private static IgniteConfiguration GetIgniteConfiguration()
+        {
+            return new IgniteConfiguration
+            {
+                DiscoverySpi = new TcpDiscoverySpi
+                {
+                    IpFinder = new TcpDiscoveryStaticIpFinder
+                    {
+                        Endpoints = new[] { "127.0.0.1:47500" }
+                    }
+                }
+            };
         }
     }
 }
