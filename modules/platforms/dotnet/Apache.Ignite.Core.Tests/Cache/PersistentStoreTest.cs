689eb11fcfa36d400e090c5c7882823b1d13b247
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-06-15 20:26:07 +0300

    IGNITE-5507 .NET: IIgnite.Active

---
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Cache/PersistentStoreTest.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Tests.Cache
+{
+    using System.Runtime.InteropServices;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests the persistent store.
+    /// </summary>
+    public class PersistentStoreTest
+    {
+        /// <summary>
+        /// Tests the grid activation.
+        /// </summary>
+        [Test]
+        public void TestGridActivation()
+        {
+            
+        }
+        
+        /// <summary>
+        /// Tests the grid activation without persistence (active by default).
+        /// </summary>
+        [Test]
+        public void TestGridActivationNoPersistence()
+        {
+            using (var ignite = Ignition.Start(TestUtils.GetTestConfiguration()))
+            {
+                Assert.IsTrue(ignite.IsActive());
+
+                ignite.SetActive(false);
+                Assert.IsFalse(ignite.IsActive());
+
+                ignite.SetActive(true);
+                Assert.IsTrue(ignite.IsActive());
+            }
+        }
+    }
+}
