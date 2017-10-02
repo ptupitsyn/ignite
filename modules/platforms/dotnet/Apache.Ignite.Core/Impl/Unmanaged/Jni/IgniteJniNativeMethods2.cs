0328e2aa28bf9b9adb64fea97468d08254566703
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-10-02 19:37:34 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core/Impl/Unmanaged/Jni/IgniteJniNativeMethods2.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Unmanaged/Jni/IgniteJniNativeMethods2.cs
@@ -15,8 +15,9 @@
  * limitations under the License.
  */
 
-namespace Apache.Ignite.Core.Impl.Unmanaged
+namespace Apache.Ignite.Core.Impl.Unmanaged.Jni
 {
+    using System;
     using System.Runtime.InteropServices;
     using System.Security;
 
@@ -32,6 +33,48 @@ namespace Apache.Ignite.Core.Impl.Unmanaged
         [DllImport(IgniteUtils.FileJvmDll, EntryPoint = "IgniteTargetInLongOutLong")]
         public static extern long TargetInLongOutLong(void* ctx, void* target, int opType, long val);
 
+        [DllImport("jvm.dll", CallingConvention = CallingConvention.StdCall)]
+        internal static extern JNIResult JNI_CreateJavaVM(out IntPtr pvm, out IntPtr penv,
+            JavaVMInitArgs* args);
+
+        [DllImport("jvm.dll", CallingConvention = CallingConvention.StdCall)]
+        internal static extern JNIResult JNI_GetCreatedJavaVMs(out IntPtr pvm, int size,
+            [Out] out int size2);
+
+        [DllImport("jvm.dll", CallingConvention = CallingConvention.StdCall)]
+        internal static extern JNIResult JNI_GetDefaultJavaVMInitArgs(JavaVMInitArgs* args);
+
+
 
     }
+
+    internal enum JNIResult
+    {
+        Success = 0,
+        Error = -1,
+        ThreadDetached = -2,
+        VersionError = -3,
+        NotEnoughMemory = -4,
+        AlreadyExists = -5,
+        InvalidArguments = -6
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 0)]
+    internal unsafe struct JavaVMInitArgs
+    {
+        public int version;
+        public int nOptions;
+        public JavaVMOption* options;
+        public byte ignoreUnrecognized;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 0)]
+    internal struct JavaVMOption
+    {
+        public IntPtr optionString;
+        public IntPtr extraInfo;
+    }
+
+
+
 }
