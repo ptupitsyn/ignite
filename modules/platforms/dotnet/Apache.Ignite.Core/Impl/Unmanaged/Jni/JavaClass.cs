a0b635520ce423c72d9e1a8ebd34eda9bdf685b4
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-10-27 17:25:15 +0300

    wip jni

---
+++ modules/platforms/dotnet/Apache.Ignite.Core/Impl/Unmanaged/Jni/JavaClass.cs
@@ -0,0 +1,12 @@
+﻿namespace Apache.Ignite.Core.Impl.Unmanaged.Jni
+{
+    internal class JavaClass
+    {
+        private readonly JniGlobalHandle _handle;
+
+        public JavaClass(JniGlobalHandle handle)
+        {
+            _handle = handle;
+        }
+    }
+}
