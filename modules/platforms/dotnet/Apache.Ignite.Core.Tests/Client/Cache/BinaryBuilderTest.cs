4904e11160f81cc36bae89c2da6712235ed4481a
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-11-29 20:13:27 +0300

    wip builder tests

---
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/Client/Cache/BinaryBuilderTest.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Tests.Client.Cache
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests binary object builder in thin client.
+    /// </summary>
+    public class BinaryBuilderTest : ClientTestBase
+    {
+        /// <summary>
+        /// Tests the classless object builder.
+        /// </summary>
+        [Test]
+        public void TestClasslessBuilder()
+        {
+            var bin = Client.GetBinary();
+
+            var obj = bin.GetBuilder("FooBarBaz")
+                .SetByteField("code", 99)
+                .SetStringField("name", "abc")
+                .Build();
+
+            var cache = GetBinaryCache();
+            cache[1] = obj;
+            var res = cache.Get(1);
+
+            Assert.AreEqual("abc", res.GetField<string>("name"));
+            Assert.AreEqual(99, res.GetField<byte>("code"));
+            Assert.IsNull(res.GetField<object>("field"));
+
+            var type = res.GetBinaryType();
+            Assert.AreEqual("FooBarBaz", type.TypeName);
+            Assert.IsFalse(type.IsEnum);
+
+            CollectionAssert.AreEquivalent(new[] { "code", "name" }, type.Fields);
+            Assert.AreEqual("byte", type.GetFieldTypeName("code"));
+            Assert.AreEqual("String", type.GetFieldTypeName("name"));
+        }
+    }
+}
