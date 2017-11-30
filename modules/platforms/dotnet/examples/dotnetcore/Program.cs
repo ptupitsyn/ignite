1a5cd0028af54e5d64b216b7bf8ca3634c04e019
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-11-30 21:33:39 +0300

    Adding runner

---
+++ modules/platforms/dotnet/examples/dotnetcore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apache.Ignite.Examples
+{
+    /// <summary>
+    /// Examples selector - program entry point.
+    /// </summary>
+    public static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
