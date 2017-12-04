872f4648a30bf54045148a329e824286ba095afb
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-12-04 17:47:08 +0300

    wip

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/ApiParity/CacheAffinityParityTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/ApiParity/CacheAffinityParityTest.cs
@@ -17,32 +17,23 @@
 
 namespace Apache.Ignite.Core.Tests.ApiParity
 {
-    using System.Collections.Generic;
-    using Apache.Ignite.Core.Binary;
+    using Apache.Ignite.Core.Cache;
     using NUnit.Framework;
 
     /// <summary>
-    /// Tests that <see cref="IBinary"/> has all APIs from Java Ignite interface.
+    /// Tests that <see cref="ICacheAffinity"/> has all APIs from Java Ignite interface.
     /// </summary>
-    public class BinaryParityTest
+    public class CacheAffinityParityTest
     {
-        /** Known name mappings. */
-        private static readonly Dictionary<string, string> KnownMappings = new Dictionary<string, string>
-        {
-            {"type", "GetBinaryType"},
-            {"types", "GetBinaryTypes"}
-        };
-
         /// <summary>
         /// Tests the API parity.
         /// </summary>
         [Test]
-        public void TestBinary()
+        public void TestCacheAffinity()
         {
             ParityTest.CheckInterfaceParity(
-                @"modules\core\src\main\java\org\apache\ignite\IgniteBinary.java",
-                typeof(IBinary),
-                knownMappings: KnownMappings);
+                @"modules\core\src\main\java\org\apache\ignite\cache\affinity\Affinity.java",
+                typeof(ICacheAffinity));
         }
     }
 }
