ad9dcdd917e80a0b885f1d064b26f0fe54ec7522
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-12-04 14:23:21 +0300

    ComputeParityTest

---
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/ApiParity/ComputeParityTest.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Tests.ApiParity
+{
+    using System.Collections.Generic;
+    using Apache.Ignite.Core.Compute;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests that <see cref="ICompute"/> has all APIs from Java Ignite interface.
+    /// </summary>
+    public class ComputeParityTest
+    {
+        /** Methods that are not needed on .NET side. */
+        private static readonly string[] UnneededMethods =
+        {
+        };
+
+        /** Members that are missing on .NET side and should be added in future. */
+        private static readonly string[] MissingProperties =
+        {
+        };
+
+        /** Known name mappings. */
+        private static readonly Dictionary<string, string> KnownMappings = new Dictionary<string, string>
+        {
+        };
+
+        /// <summary>
+        /// Tests the IIgnite parity.
+        /// </summary>
+        [Test]
+        public void TestIgnite()
+        {
+            ParityTest.CheckInterfaceParity(
+                @"modules\core\src\main\java\org\apache\ignite\IgniteCompute.java",
+                typeof(ICompute),
+                UnneededMethods,
+                MissingProperties,
+                KnownMappings);
+        }
+    }
+}
