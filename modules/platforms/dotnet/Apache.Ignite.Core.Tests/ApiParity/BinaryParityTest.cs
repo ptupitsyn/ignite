b16fcfafd688b84269cf4c9d06b8cd6243f90d40
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-12-04 17:23:45 +0300

    wip binary parity

---
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/ApiParity/BinaryParityTest.cs
@@ -0,0 +1,32 @@
+﻿namespace Apache.Ignite.Core.Tests.ApiParity
+{
+    using System.Collections.Generic;
+    using Apache.Ignite.Core.Binary;
+    using Apache.Ignite.Core.Datastream;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests that <see cref="IBinary"/> has all APIs from Java Ignite interface.
+    /// </summary>
+    public class BinaryParityTest
+    {
+        /** Known name mappings. */
+        private static readonly Dictionary<string, string> KnownMappings = new Dictionary<string, string>
+        {
+            {"type", "GetType"},
+            {"types", "GetTypes"}
+        };
+
+        /// <summary>
+        /// Tests the API parity.
+        /// </summary>
+        [Test]
+        public void TestBinary()
+        {
+            ParityTest.CheckInterfaceParity(
+                @"modules\core\src\main\java\org\apache\ignite\IgniteBinary.java",
+                typeof(IBinary),
+                knownMappings: KnownMappings);
+        }
+    }
+}
