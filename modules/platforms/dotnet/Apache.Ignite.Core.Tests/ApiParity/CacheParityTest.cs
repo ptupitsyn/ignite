537cffd73b49b3fcc7181176fd360b3af787adda
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-12-04 14:58:30 +0300

    wip cache test

--- modules/platforms/dotnet/Apache.Ignite.Core.Tests/ApiParity/CacheParityTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Core.Tests/ApiParity/CacheParityTest.cs
@@ -1,4 +1,21 @@
-﻿namespace Apache.Ignite.Core.Tests.ApiParity
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Core.Tests.ApiParity
 {
     using System.Collections.Generic;
     using Apache.Ignite.Core.Cache;
@@ -9,13 +26,22 @@
     /// </summary>
     public class CacheParityTest
     {
-        /** Methods that are not needed on .NET side. */
-        private static readonly string[] UnneededMethods =
+        /** Members that are not needed on .NET side. */
+        private static readonly string[] UnneededMembers =
         {
+            "close",
+            "mxBean",
+            "localMxBean",
+
+            // The following look pointless, same as get, getAll, etc:
+            "entry",
+            "entryAsync",
+            "entries",
+            "entriesAsync"
         };
 
         /** Members that are missing on .NET side and should be added in future. */
-        private static readonly string[] MissingProperties =
+        private static readonly string[] MissingMembers =
         {
         };
 
@@ -33,8 +59,8 @@
             ParityTest.CheckInterfaceParity(
                 @"modules\core\src\main\java\org\apache\ignite\IgniteCache.java",
                 typeof(ICache<,>),
-                UnneededMethods,
-                MissingProperties,
+                UnneededMembers,
+                MissingMembers,
                 KnownMappings);
         }
     }
