27ff4ef1f79b3b96ecdcc94484ab5f4f21b177d7
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-12-22 16:14:20 +0300

    IGNITE-7288 Thin client: partial cache configuration

--- modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/cache/ClientCacheConfigurationSerializer.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/platform/client/cache/ClientCacheConfigurationSerializer.java
@@ -40,6 +40,99 @@ import static org.apache.ignite.internal.processors.platform.utils.PlatformConfi
  * Cache configuration serializer.
  */
 public class ClientCacheConfigurationSerializer {
+    /** */
+    private static final short ATOMICITY_MODE = 0;
+
+    /** */
+    private static final short BACKUPS = 1;
+
+    /** */
+    private static final short CACHE_MODE = 2;
+
+    /** */
+    private static final short COPY_ON_READ = 3;
+
+    /** */
+    private static final short DATA_REGION_NAME = 4;
+
+    /** */
+    private static final short EAGER_TTL = 5;
+
+    /** */
+    private static final short STATISTICS_ENABLED = 6;
+
+    /** */
+    private static final short GROUP_NAME = 7;
+
+    /** */
+    private static final short INVALIDATE = 8;
+
+    /** */
+    private static final short DEFAULT_LOCK_TIMEOUT = 9;
+
+    /** */
+    private static final short MAX_CONCURRENT_ASYNC_OPERATIONS = 10;
+
+    /** */
+    private static final short MAX_QUERY_ITERATORS_COUNT = 11;
+
+    /** */
+    private static final short NAME = 12;
+
+    /** */
+    private static final short ONHEAP_CACHE_ENABLED = 13;
+
+    /** */
+    private static final short PARTITION_LOSS_POLICY = 14;
+
+    /** */
+    private static final short QUERY_DETAIL_METRICS_SIZE = 15;
+
+    /** */
+    private static final short QUERY_PARALLELISM = 16;
+
+    /** */
+    private static final short READ_FROM_BACKUP = 17;
+
+    /** */
+    private static final short REBALANCE_BATCH_SIZE = 18;
+
+    /** */
+    private static final short REBALANCE_BATCHES_PREFETCH_COUNT = 19;
+
+    /** */
+    private static final short REBALANCE_DELAY = 20;
+
+    /** */
+    private static final short REBALANCE_MODE = 21;
+
+    /** */
+    private static final short REBALANCE_ORDER = 22;
+
+    /** */
+    private static final short REBALANCE_THROTTLE = 23;
+
+    /** */
+    private static final short REBALANCE_TIMEOUT = 24;
+
+    /** */
+    private static final short SQL_ESCAPE_ALL = 25;
+
+    /** */
+    private static final short SQL_INDEX_MAX_INLINE_SIZE = 26;
+
+    /** */
+    private static final short SQL_SCHEMA = 27;
+
+    /** */
+    private static final short WRITE_SYNCHRONIZATION_MODE = 28;
+
+    /** */
+    private static final short KEY_CONFIGURATION = 29;
+
+    /** */
+    private static final short QUERY_ENTITIES = 30;
+
     /**
      * Writes the cache configuration.
      * @param writer Writer.
