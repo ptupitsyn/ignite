ece8652ad376f67e10f54d082cf514561ff62955
Pavel Tupitsyn <ptupitsyn@apache.org>
2018-01-30 17:56:23 +0300

    wip

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -27,4 +27,4 @@ $ver = "2.5.0"
 # TODO
 
 # .NET: update AssemblyInfo files.
-grep -r --include "AssemblyInfo.cs" | xargs sed -i 's/[0-9]\.[0-9]\.0\./2.5.0'
+find . -name "AssemblyInfo.cs" | xargs sed -i 's/[0-9]\.[0-9]\.0\./2.5.0'
