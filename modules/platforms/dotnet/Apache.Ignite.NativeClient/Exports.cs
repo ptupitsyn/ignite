4d820a41759547701c4e3d3d7d5d00345bcbe0c0
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-11-16 22:01:18 +0200

    wip Exports

---
+++ modules/platforms/dotnet/Apache.Ignite.NativeClient/Exports.cs
@@ -0,0 +1,9 @@
+﻿namespace Apache.Ignite.NativeClient;
+
+using System.Runtime.InteropServices;
+
+public static class Exports
+{
+    [UnmanagedCallersOnly(EntryPoint = "AddOne")]
+    public static int AddOne(int i) => i + 1;
+}
